546d811a3b903c058053551d8975b5828a48b226
Rahat Ahmed <rahat@ndessl.local>
2024-02-04 19:12:19 +0600

    variables running

---
+++ variable/variable.js
@@ -0,0 +1,11 @@
+document.getElementById("title").innerHTML="Hello This is variable";
+let fruits= 'Mango'
+console.log(`${fruits} is my favourite fruit and I like to eat ${fruits} `);
+
+//Var variable
+var a = 10;
+var a =12;
+console.log(a); //var is redeclare
+a = 'Bangladesh'; // var is reassign
+console.log(a);
+var country; //undefined
