2be15d2cea7d4a3d487ec27f1454f67d2aa29641
Rahat Ahmed <rahat@ndessl.local>
2024-02-04 19:26:41 +0600

    Variable complete

--- variable/variable.js
+++ variable/variable.js
@@ -9,3 +9,22 @@ console.log(a); //var is redeclare
 a = 'Bangladesh'; // var is reassign
 console.log(a);
 var country; //undefined
+
+//Let variable 
+let b =10; //let is not redeclare
+b=11; // let is reassign
+console.log(b);
+
+//const variable
+const c =15; // const is not redeclare
+//c=16; // const is not reassign
+console.log(c);
+
+const person = {
+    firstName: 'Rahat',
+    age : 30
+
+};
+person.country= 'Bangladesh';
+person.age= '32';
+console.log(person);
