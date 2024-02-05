2c48702b62f79bc3eaf8fe09822145c3e9cb7461
Rahat Ahmed <rahat@ndessl.local>
2024-02-05 19:07:19 +0600

    dataType complete

---
+++ DataTypes/datatypes.js
@@ -0,0 +1,13 @@
+//Variable in a string
+
+let lastName = 'Tanjim';
+console.log(`My name is ${lastName}`);
+
+let age=30;
+let age2=21;
+let calculation1= age + age2+ lastName;
+console.log(calculation1);
+console.log(typeof(calculation1));
+let calculation2= age + age2 + lastName + age + age2;
+console.log(calculation2);
+
